5f765397ae3e2a2d2707d1d45b9f1312434afe8a
Muhammad Farhan <farhanshah.96930012@gmail.com>
2019-07-13 03:23:17 +0530

    cors access-control-allow-origin allowed for all origins

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const winston = require('winston')
-const express = require('express')
-const app = express()
+const winston = require('winston');
+const express = require('express');
+const app = express();
 
-require('./startup/logging')()
-require('./startup/routes')(app)
-require('./startup/db')()
-require('./startup/config')()
-require('./startup/validation')()
-require('./startup/prod')(app)
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
-const port = process.env.PORT || 3000
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
+require('./startup/prod')(app);
 
-module.exports = server
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
